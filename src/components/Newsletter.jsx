676b8ce3adb71d9d099de4636b1c0dff7a01d8b7
khushi303 <maankhushi303@gmail.com>
2024-03-07 14:24:53 +0530

    change in input

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -8,7 +8,7 @@ const Newsletter = () => {
                 <h2 className='ff_outfit fw-semibold fs_lg text-center text-white lh_120 mb-0'>Subscribe Newsletter</h2>
                 <p className='ff_outfit fw-normal fs_2sm text-center text-white op_90 lh_151 mt-3 mb-38'>Subscribe to our newsletter for the latest news, offers, and eco-tips.</p>
                 <div className='mt-4 bor_email mx-auto d-flex'>
-                    <input type="text" placeholder='Enter your email' className='w-100 border-0 input_mail ff_outfit fw-normal fs_2sm op_80 bg-transparent pr_20 text-white' />
+                    <input type="text" placeholder='Enter your email' className='w-100 border-0 input_mail ff_outfit fw-normal fs_2sm op_80 bg-transparent pr_20 text-white pe-3' />
                     <button className='btn_mail border-0 bg-white ff_outfit fw-semibold fs_2sm text_blue'>Join</button>
                 </div>
             </div>
